9e7524218e19e6f24f306c3f04f1bc4fe064697f
Francisco Alegria <mr.alegria42@protonmail.com>
2022-06-03 03:35:45 -0400

    alt

--- routes/apiRoutes/index.js
+++ routes/apiRoutes/index.js
@@ -1,6 +1,6 @@
 // required items
 const router = require('express').Router();
-const noteRoutes = require('../apiRoutes/noteRoutes.js');
+const noteRoutes = require('./noteRoutes');
 
 // use noteRoutes.js
 router.use(noteRoutes);
